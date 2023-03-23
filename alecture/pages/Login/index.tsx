022f79bdb0802a25a948330974b8bf8761aabc95
Glenn <needermj@gmail.com>
2023-03-23 18:12:27 +0900

    Feat: connect signup/login api

--- alecture/pages/Login/index.tsx
+++ alecture/pages/Login/index.tsx
@@ -1,9 +1,71 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
+import { Success, Form, Error, Label, Input, LinkContainer, Button, Header } from '@pages/SignUp/styles';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import useInput from '@hooks/useInput';
 
 const LogIn = () => {
+  const [email, onChangeEmail] = useInput('');
+  const [password, onChangePassword] = useInput('');
+  const [logInError, setLogInError] = useState(false);
+
+  const onSubmit = useCallback(
+    (e) => {
+      e.preventDefault();
+      console.log({ email, password });
+      setLogInError(false);
+      axios
+        .post('/api/users/login', { email, password })
+        .then((res) => {
+          console.log(res);
+        })
+        .catch((err) => {
+          setLogInError(true);
+        })
+        .finally(() => {});
+    },
+    [email, password],
+  );
+
   return (
-    <div>로그인</div>
-  )
+    <div id="container">
+      <Header>Sleact</Header>
+      <Form onSubmit={onSubmit}>
+        <Label id="email-label">
+          <span>이메일 주소</span>
+          <div>
+            <Input
+              type="email"
+              id="email"
+              name="email"
+              value={email}
+              onChange={onChangeEmail}
+              placeholder="이메일 주소를 입력해주세요"
+            />
+          </div>
+        </Label>
+        <Label id="password-label">
+          <span>비밀번호</span>
+          <div>
+            <Input
+              type="password"
+              id="password"
+              name="password"
+              value={password}
+              onChange={onChangePassword}
+              placeholder="비밀번호를 입력해주세요"
+            />
+          </div>
+          {logInError && <Error>이메일과 비밀번호 조합이 일치하지 않습니다.</Error>}
+        </Label>
+        <Button type="submit">로그인</Button>
+      </Form>
+      <LinkContainer>
+        아직 회원이 아니신가요?&nbsp;
+        <Link to="/signup">회원가입 하러가기</Link>
+      </LinkContainer>
+    </div>
+  );
 };
 
 export default LogIn;
--- alecture/pages/SignUp/index.tsx
+++ alecture/pages/SignUp/index.tsx
@@ -1,6 +1,7 @@
 import React, { useCallback, useState } from 'react';
 import { Success, Form, Error, Label, Input, LinkContainer, Button, Header } from './styles';
-import { Link, Redirect } from 'react-router-dom';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 import useInput from '@hooks/useInput';
 
 const SignUp = () => {
@@ -9,6 +10,8 @@ const SignUp = () => {
   const [password, , setPassword] = useInput('');
   const [passwordCheck, , setPasswordCheck] = useInput('');
   const [mismatchError, setMismatchError] = useState(false);
+  const [signUpError, setSignUpError] = useState('');
+  const [signUpSuccess, setSignUpSuccess] = useState(false);
 
   const onChangePassword = useCallback(
     (e) => {
@@ -29,9 +32,23 @@ const SignUp = () => {
   const onSubmit = useCallback(
     (e) => {
       e.preventDefault();
-      console.log({ email, nickname, password, passwordCheck });
       if (!mismatchError) {
         console.log('회원가입');
+        setSignUpError('');
+        setSignUpSuccess(false);
+        axios
+          .post('/api/users', {
+            email,
+            nickname,
+            password,
+          })
+          .then(() => {
+            setSignUpSuccess(true);
+          })
+          .catch((err) => {
+            setSignUpError(err.response.data);
+          })
+          .finally(() => {});
       }
     },
     [email, nickname, password, passwordCheck],
@@ -94,8 +111,8 @@ const SignUp = () => {
           </div>
           {mismatchError && <Error>비밀번호가 일치하지 않습니다.</Error>}
           {!nickname && <Error>닉네임을 입력해주세요.</Error>}
-          {/* {signUpError && <Error>{signUpError}</Error>}
-          {signUpSuccess && <Success>회원가입되었습니다! 로그인해주세요.</Success>} */}
+          {signUpError && <Error>{signUpError}</Error>}
+          {signUpSuccess && <Success>회원가입되었습니다! 로그인해주세요.</Success>}
         </Label>
         <Button type="submit">회원가입</Button>
       </Form>
